643fb961c89478b9b1f8aca4067955166a13d0dc
swd <stphndcky22@gmail.com>
2022-05-09 15:03:16 -0700

    added some routes to index file

---
+++ controllers/api/index.js
@@ -0,0 +1,24 @@
+// import router from Express.js
+const router = require('express').Router();
+
+
+// import our GET, POST, DELETE, and PUT requests for users
+const userRoutes = require('./user-routes.js');
+
+///////////////////////////////////////////
+
+
+// we write /users so all of our userRoutes endpoints have this before
+router.use('/users', userRoutes);
+
+//router.use('/options', optionRoutes);
+
+//router.use('/questions', questionRoutes);
+
+
+
+//////////////////////////////////////////
+
+
+// we must export this file so it can be attached in server.js
+module.exports = router;
